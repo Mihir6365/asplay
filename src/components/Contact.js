bea208bae28bd14d7f7c02749de79b052c7b2310
Mihir6365 <savla.mp@somaiya.edu>
2023-01-05 17:41:36 +0530

    form test

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -52,7 +52,7 @@ export const Contact = () => {
               {({ isVisible }) =>
                 <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                   <h2>Get In Touch</h2>
-                  <form onSubmit={handleSubmit}>
+                  <form onSubmit={handleSubmit} name="contact" netlify>
                     <Row>
                       <Col size={12} sm={6} className="px-1">
                         <input type="text" placeholder="First Name" />
