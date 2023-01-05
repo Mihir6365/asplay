0940d2e0942c4c0f288c68958a8cdc076a6cb848
Mihir6365 <savla.mp@somaiya.edu>
2023-01-05 13:30:09 +0530

    removed backend

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import { MailchimpForm } from "./MailchimpForm";
 import logo from "../assets/img/logo.svg";
 import navIcon1 from "../assets/img/nav-icon1.svg";
